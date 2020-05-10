82787b8b5419169b7549fc7101eee1b8c60418cd
gopi-g <gopi.gg@gmail.com>
2020-05-10 16:48:51 +0530

    Function updated with code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The function makeCacheMatrix creates list of functions can following
+# 1.	set the value of the given matrix
+# 2.	get the value of the given matrix
+# 3.	set the value of the inverse of the given matrix
+# 4.	get the value of the inverse of the given matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv1 <- NULL
+  setmatrix <- function(m) {
+    x <<- m
+    inv1 <<- NULL
+  }
+  getmatrix <- function() x
+  setinv <- function(inv) inv1 <<- inv
+  getinv <- function() inv1
+  list(setmatrix = setmatrix, getmatrix = getmatrix,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The function CacheSolve takes the makeCacheMatrix function as a argument
+## and check whether inverse matrix is cached,  
+## if it is not it calculates inverse and cache it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached inverse matrix ")
+    return(inv)
+  }
+  data <- x$getmatrix()
+  if (det(data) == 0){
+    message("Given matrix is singular, Hence it is not invertable")
+  }
+  else{  
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv}
 }
